3597bcc578434977c5575f3dc76ef74f10278713
Your Name01~ <you@example.com>
2024-05-29 22:21:57 +0700

    fix tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,15 +4,15 @@ import { I18nextProvider } from "react-i18next";
 import i18nForTests from 'shared/config/i18n/i18nForTesting';
 import { MemoryRouter } from "react-router-dom";
 
-
-export interface renderWithTranslationOption {
+export interface componentRenderOption {
   route?: string
 }
-export function renderWithTranslation(component: ReactNode, options: renderWithTranslationOption = {}) {
+
+export function componentRender(component: ReactNode, options: componentRenderOption = {}) {
   const { route = '/' } = options
   return render(
     <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests} >
+      <I18nextProvider i18n={i18nForTests}>
         {component}
       </I18nextProvider>
     </MemoryRouter>
