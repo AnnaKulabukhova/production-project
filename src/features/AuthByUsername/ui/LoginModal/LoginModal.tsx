644645bfd57fa9801fca74f54f0f6b164aa3d755
Your Name01~ <you@example.com>
2024-06-05 16:13:05 +0700

    add pre-commit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,17 +1,14 @@
 import { Modal } from 'shared/ui/Modal'
 import { LoginForm } from '../LoginForm/LoginForm'
-import classes from './LoginModal.module.scss'
-import { classNames } from "shared/lib/classNames/classNames"
 
 interface LoginModalProps {
-  className?: string
   isOpen?: boolean,
   onClose?: () => void
 }
 
-export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
+export const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
   return (
-    <Modal lazy isOpen={isOpen} onClose={onClose} className={classNames(classes.loginModal, {}, [className ?? ''])} >
+    <Modal lazy isOpen={isOpen} onClose={onClose} >
       <LoginForm />
     </Modal>
   )
